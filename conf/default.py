99219693f512e5fc67b8a13d1db39875166e346e
unknown <gujingy@gd.ctc.com>
2018-08-27 19:34:50 +0800

    update

--- conf/default.py
+++ conf/default.py
@@ -30,8 +30,8 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = 'hellotest'
-APP_TOKEN = 'e3995ed1-5d14-4d1b-bd30-7fa0c4f4e572'
+APP_ID = 'jiajyhello'
+APP_TOKEN = 'b5ca7018-a638-462a-98a4-f8da5685e96f'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'http://paas.blueking.com:80'
 
--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'root',                        # 你的数据库password
-        'HOST': '192.168.146.100',                   		   # 数据库HOST
+        'NAME': 'jjyhellotest',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'jjyhellotest',                            # 你的数据库user
+        'PASSWORD': 'jjyhellotest@2018',                        # 你的数据库password
+        'HOST': '172.50.18.26',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
